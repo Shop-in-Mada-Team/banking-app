cb7cebb3c0dff8622a7890fd5d7f748b68367325
bensonirah <bensonirah@gmail.com>
2024-01-18 20:48:35 +0300

    Implement transaction log operation

--- src/Domain/Entity/BankAccount.php
+++ src/Domain/Entity/BankAccount.php
@@ -2,6 +2,7 @@
 
 namespace Shopinmada\BankingApp\Domain\Entity;
 
+use Shopinmada\BankingApp\Domain\Enum\TransactionType;
 use Shopinmada\BankingApp\Domain\ValueObject\BankAccountId;
 use Shopinmada\BankingApp\Domain\ValueObject\MoneyInterface;
 
@@ -9,6 +10,7 @@ class BankAccount
 {
     private BankAccountId $bankAccountId;
     private MoneyInterface $money;
+    private array $bankTransactions;
 
     /**
      * @param BankAccountId $bankAccountId
@@ -18,6 +20,7 @@ class BankAccount
     {
         $this->bankAccountId = $bankAccountId;
         $this->money = $money;
+        $this->bankTransactions = [];
     }
 
     public function getId(): BankAccountId
@@ -36,6 +39,8 @@ class BankAccount
     public function deposit(MoneyInterface $money): void
     {
         $this->money->add($money);
+        $bankTransaction = new BankTransaction(bankAccount: $this, transactionType: TransactionType::DEPOSIT, amount: $money);
+        $this->bankTransactions[] = $bankTransaction;
     }
 
     public function retrait(MoneyInterface $money): void
@@ -44,13 +49,23 @@ class BankAccount
             throw new \InvalidArgumentException("Votre solde est insuffisante!");
         }
         $this->money->subtracts($money);
+        $bankTransaction = new BankTransaction(bankAccount: $this, transactionType: TransactionType::RETRAIT, amount: $money);
+        $this->bankTransactions[] = $bankTransaction;
     }
 
     public function transfert(BankAccount $recipientAccount, MoneyInterface $money): void
     {
         $this->retrait($money);
         $recipientAccount->deposit($money);
+        $bankTransaction = new BankTransaction(bankAccount: $this, transactionType: TransactionType::TRANSFER, amount: $money);
+        $this->bankTransactions[] = $bankTransaction;
     }
+
+    public function transactions(): array
+    {
+        return $this->bankTransactions;
+    }
+
     public function __toString(): string
     {
         return sprintf("BankAccount # %s : Ar %d", $this->bankAccountId, $this->getAmount());
