da9358274e164d943dea4122d46f78bd5c6e8fb1
bensonirah <bensonirah@gmail.com>
2024-01-19 17:02:58 +0300

    add transaction reason

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 
 use Shopinmada\BankingApp\Domain\ValueObject\BankAccountId;
+use Shopinmada\BankingApp\Domain\ValueObject\Money;
 use Shopinmada\BankingApp\Repository\BankAccountInMemoryRepository;
 use Shopinmada\BankingApp\Service\BankAccountService;
 
@@ -14,5 +15,8 @@ dump($bankAccountId);
 
 sleep(5);
 $bankAccount = $bankAccountRepository->get(BankAccountId::fromUuid($bankAccountId));
-
+$bankAccount->deposit(Money::fromAmount('MGA', 7000), 'Virement salaire mois de janvier!');
+$bankAccount->deposit(Money::fromAmount('MGA', 7000), 'Rembouressement frais médicaux');
+dump($bankAccount->countTransaction());
+dump($bankAccount->transactions());
 dump($bankAccount . '');
